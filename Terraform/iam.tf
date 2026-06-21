d9e86f9152421184eb4049e0e6b7e4ef927a9f0e
cva44 <shivakrishna.vosukula@gmail.com>
2026-06-21 08:00:57 +0530

    script

--- Terraform/iam.tf
+++ Terraform/iam.tf
@@ -42,77 +42,77 @@ resource "aws_iam_role_policy_attachment" "registry" {
   role       = aws_iam_role.node_group.name
 }
 
-# resource "aws_iam_role_policy_attachment" "ebs_csi" {
-#   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-#   role       = aws_iam_role.node_group.name
-# }
+resource "aws_iam_role_policy_attachment" "ebs_csi" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+  role       = aws_iam_role.node_group.name
+}
 
-# resource "aws_iam_role_policy_attachment" "ecr_power_user" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
-#   role       = aws_iam_role.node_group.name
-# }
+resource "aws_iam_role_policy_attachment" "ecr_power_user" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
+  role       = aws_iam_role.node_group.name
+}
 
-# # ==========================================
-# # 2. Dedicated IRSA Role for AWS EBS CSI Driver Addon
-# # ==========================================
-# resource "aws_iam_role" "ebs_csi_role" {
-#   name = "${var.project_name}-${var.environment}-ebs-csi-role"
+# ==========================================
+# 2. Dedicated IRSA Role for AWS EBS CSI Driver Addon
+# ==========================================
+resource "aws_iam_role" "ebs_csi_role" {
+  name = "${var.project_name}-${var.environment}-ebs-csi-role"
 
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRoleWithWebIdentity"
-#         Effect = "Allow"
-#         Principal = {
-#           Federated = module.eks.oidc_provider_arn
-#         }
-#         Condition = {
-#           StringEquals = {
-#             "${module.eks.oidc_provider}:sub" = "system:serviceaccount:kube-system:ebs-csi-controller-sa"
-#           }
-#         }
-#       }
-#     ]
-#   })
-# }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Effect = "Allow"
+        Principal = {
+          Federated = module.eks.oidc_provider_arn
+        }
+        Condition = {
+          StringEquals = {
+            "${module.eks.oidc_provider}:sub" = "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+          }
+        }
+      }
+    ]
+  })
+}
 
-# # Attach permissions to the dedicated service account role
-# resource "aws_iam_role_policy_attachment" "ebs_csi_role_policy" {
-#   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-#   role       = aws_iam_role.ebs_csi_role.name
-# }
+# Attach permissions to the dedicated service account role
+resource "aws_iam_role_policy_attachment" "ebs_csi_role_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+  role       = aws_iam_role.ebs_csi_role.name
+}
 
-# # IAM Role for AWS Load Balancer Controller
-# resource "aws_iam_role" "alb_controller_role" {
-#   name = "${var.project_name}-${var.environment}-alb-controller-role"
+# IAM Role for AWS Load Balancer Controller
+resource "aws_iam_role" "alb_controller_role" {
+  name = "${var.project_name}-${var.environment}-alb-controller-role"
 
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRoleWithWebIdentity"
-#         Effect = "Allow"
-#         Principal = {
-#           Federated = module.eks.oidc_provider_arn
-#         }
-#         Condition = {
-#           StringEquals = {
-#             "${module.eks.oidc_provider}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
-#           }
-#         }
-#       }
-#     ]
-#   })
-# }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Effect = "Allow"
+        Principal = {
+          Federated = module.eks.oidc_provider_arn
+        }
+        Condition = {
+          StringEquals = {
+            "${module.eks.oidc_provider}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
+          }
+        }
+      }
+    ]
+  })
+}
 
-# # Attach full structural network integration permissions
-# # resource "aws_iam_role_policy_attachment" "alb_controller_policy" {
-# #   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSLoadBalancingPolicy" # Assumes policy exists, or use standard full policy
-# #   role       = aws_iam_role.alb_controller_role.name
-# # }
+#Attach full structural network integration permissions
+resource "aws_iam_role_policy_attachment" "alb_controller_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSLoadBalancingPolicy" # Assumes policy exists, or use standard full policy
+  role       = aws_iam_role.alb_controller_role.name
+}
 
-# # # Output the ARN so we can see it easily
-# # output "alb_controller_role_arn" {
-# #   value = aws_iam_role.alb_controller_role.arn
-# # }
+# Output the ARN so we can see it easily
+output "alb_controller_role_arn" {
+  value = aws_iam_role.alb_controller_role.arn
+}
